e8d76e1c16cae989f81a2be1dba8a35efd0906ba
Mitchell Adair <madair@singlestore.com>
2024-07-22 13:55:12 -0400

    fix app schema

--- test/test1/schema.ts
+++ test/test1/schema.ts
@@ -1,10 +1,13 @@
-import { index, int, singlestoreTable, bigint, varchar } from '../../src/singlestore-core';
+import { sql } from 'drizzle-orm';
+import { uuid } from '../../src/columns/uuid';
+import { index, singlestoreTable, bigint, varchar } from '../../src/singlestore-core';
 
 export const users = singlestoreTable('users', {
   id: bigint('id', { mode: 'number' }).primaryKey().autoincrement(),
   fullName: varchar('full_name', { length: 256 }),
-}, (users: User[]) => ({
-  nameIdx: index('name_idx').on(users.fullName),
+  extID: uuid("external_id").$default(() => sql`UUID()`)
+}, (table) => ({
+  nameIdx: index('name_idx').on(table.fullName),
 }));
 
 export type User = typeof users.$inferSelect; // return type when queried
