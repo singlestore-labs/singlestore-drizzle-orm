075a98f7735bff056375d4841cb050544077d1db
Diogo Rodrigues <drodrigues@singlestore.com>
2024-07-26 16:51:57 +0100

    Fixed migrations

--- migrations/0000_lush_night_thrasher.sql
+++ migrations/0000_lush_night_thrasher.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `comment` (
 	`id` bigint AUTO_INCREMENT NOT NULL,
-	`created_on` timestamp(6) NOT NULL DEFAULT now(6),
-	`content` varchar(256) NOT NULL,
+	`created_on` datetime(6) NOT NULL DEFAULT now(6),
+	`content` text NOT NULL,
 	`post_id` bigint NOT NULL,
 	`replies_to_comment` bigint,
 	CONSTRAINT `comment_id` PRIMARY KEY(`id`)
@@ -9,7 +9,7 @@ CREATE TABLE `comment` (
 --> statement-breakpoint
 CREATE TABLE `post` (
 	`id` bigint AUTO_INCREMENT NOT NULL,
-	`created_on` timestamp(6) NOT NULL DEFAULT now(6),
-	`content` varchar(256) NOT NULL,
+	`created_on` datetime(6) NOT NULL DEFAULT now(6),
+	`content` text NOT NULL,
 	CONSTRAINT `post_id` PRIMARY KEY(`id`)
 );
